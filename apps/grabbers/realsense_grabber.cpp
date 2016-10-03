3f352c048918e1cc80177a662862961b718362bf
Sergey Alexandrov <alexandrov88@gmail.com>
2016-10-03 21:51:40 +0200

    Add a grabber for RealSense cameras

---
+++ apps/grabbers/realsense_grabber.cpp
@@ -0,0 +1,143 @@
+/******************************************************************************
+ * Copyright (c) 2016 Sergey Alexandrov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#include <chrono>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/throw_exception.hpp>
+
+#include <librealsense/rs.hpp>
+
+#include <grabbers/realsense_grabber.h>
+
+namespace grabbers {
+
+struct RealSenseGrabber::Impl {
+  rs::context ctx;
+  rs::device* device;
+
+  cv::Size color_image_resolution = {0, 0};
+  int next_frame_index = 0;
+
+  uint64_t timestamp_zero_offset = 0;
+
+  Impl() {
+    if (ctx.get_device_count() == 0)
+      BOOST_THROW_EXCEPTION(GrabberException("No RealSense devices connected"));
+
+    device = ctx.get_device(0);
+    // device->enable_stream(rs::stream::color, rs::preset::best_quality);
+    device->enable_stream(rs::stream::color, 640, 480, rs::format::bgr8, 30);
+
+    if (!device->is_stream_enabled(rs::stream::color) ||
+        device->get_stream_format(rs::stream::color) != rs::format::bgr8)
+      BOOST_THROW_EXCEPTION(GrabberException("Failed to create color stream with requested format"));
+
+    color_image_resolution.width = device->get_stream_width(rs::stream::color);
+    color_image_resolution.height = device->get_stream_height(rs::stream::color);
+
+    device->start();
+  }
+
+  ~Impl() {
+    if (device->is_streaming())
+      device->stop();
+    device->disable_stream(rs::stream::color);
+  }
+
+  double grabFrame(cv::Mat& color) {
+    device->wait_for_frames();
+
+    auto color_data = device->get_frame_data(rs::stream::color);
+    memcpy(color.data, color_data, color.total() * color.elemSize());
+
+    ++next_frame_index;
+    return computeTimestamp(device->get_frame_timestamp(rs::stream::color));
+  }
+
+  double computeTimestamp(int device_timestamp) {
+    if (timestamp_zero_offset == 0) {
+      auto now = std::chrono::high_resolution_clock::now();
+      auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+      timestamp_zero_offset = ms - device_timestamp;
+    }
+    return (timestamp_zero_offset + device_timestamp) * 0.001;
+  }
+};
+
+RealSenseGrabber::RealSenseGrabber(const std::string& /*device_uri*/) : p(new Impl) {
+}
+
+RealSenseGrabber::~RealSenseGrabber() {}
+
+inline bool RealSenseGrabber::hasMoreFrames() const {
+  return true;
+}
+
+bool RealSenseGrabber::grabFrame(cv::OutputArray _color) {
+  if (_color.kind() != cv::_InputArray::MAT)
+    BOOST_THROW_EXCEPTION(GrabberException("Grabbing only into cv::Mat"));
+
+  _color.create(p->color_image_resolution.height, p->color_image_resolution.width, CV_8UC3);
+  cv::Mat color = _color.getMat();
+
+  return p->grabFrame(color);
+}
+
+void RealSenseGrabber::setAutoWhiteBalanceEnabled(bool state) {
+  p->device->set_option(rs::option::color_enable_auto_white_balance, state ? 1.0 : 0.0);
+}
+
+void RealSenseGrabber::setAutoExposureEnabled(bool state) {
+  p->device->set_option(rs::option::color_enable_auto_exposure, state ? 1.0 : 0.0);
+}
+
+void RealSenseGrabber::setExposure(int exposure) {
+  p->device->set_option(rs::option::color_exposure, exposure);
+}
+
+int RealSenseGrabber::getExposure() const {
+  return p->device->get_option(rs::option::color_exposure);
+}
+
+std::pair<int, int> RealSenseGrabber::getExposureRange() const {
+  double min, max, step;
+  p->device->get_option_range(rs::option::color_exposure, min, max, step);
+  return {min, max};
+}
+
+std::string RealSenseGrabber::getCameraModelName() const {
+  std::string expected_prefix = "Intel RealSense ";
+  std::string name = p->device->get_name();
+  auto pos = name.find(expected_prefix);
+  if (pos == std::string::npos)
+    BOOST_THROW_EXCEPTION(GrabberException("Unable to extract camera model name from: " + name));
+  name = name.substr(pos + expected_prefix.size());
+  boost::algorithm::to_lower(name);
+  return name;
+}
+
+std::string RealSenseGrabber::getCameraSerialNumber() const {
+  return std::string(p->device->get_serial());
+}
+
+}  // namespace grabbers
---
+++ cmake/modules/FindRealSense.cmake
@@ -0,0 +1,61 @@
+###############################################################################
+# Find librealsense
+#
+#     find_package(RealSense)
+#
+# Variables defined by this module:
+#
+#  REALSENSE_FOUND                 True if librealsense was found
+#  REALSENSE_API_VERSION           The API version
+#  REALSENSE_INCLUDE_DIR           The location of librealsense headers
+#  REALSENSE_LIBRARY               Libraries needed to use librealsense
+#
+# Imported targets defined by this module:
+#
+#  realsense                       Library to link against
+
+find_path(_REALSENSE_INCLUDE_DIR NAMES rs.h
+          HINTS /usr/local/include/librealsense
+                /usr/include/librealsense
+                /usr/local/include
+                /usr/local
+          DOC "librealsense include directory"
+)
+
+if(_REALSENSE_INCLUDE_DIR)
+
+  # Include directory
+  get_filename_component(REALSENSE_INCLUDE_DIR ${_REALSENSE_INCLUDE_DIR} DIRECTORY)
+  mark_as_advanced(REALSENSE_INCLUDE_DIR)
+
+  ## Library
+  find_library(REALSENSE_LIBRARY NAMES realsense)
+
+  if(REALSENSE_LIBRARY)
+    add_library(realsense SHARED IMPORTED)
+    set_property(TARGET realsense PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${REALSENSE_INCLUDE_DIR})
+    set_property(TARGET realsense PROPERTY IMPORTED_LOCATION ${REALSENSE_LIBRARY})
+    set(HAVE_REALSENSE TRUE)
+  endif()
+
+  # Version
+  set(REALSENSE_VERSION 0)
+  file(READ "${_REALSENSE_INCLUDE_DIR}/rs.h" _rs_H_CONTENTS)
+  string(REGEX MATCH "define[ \t]+RS_API_MAJOR_VERSION[ \t]+([0-9]+)" _major_version_match "${_rs_H_CONTENTS}")
+  set(REALSENSE_API_MAJOR_VERSION "${CMAKE_MATCH_1}")
+  string(REGEX MATCH "define[ \t]+RS_API_MINOR_VERSION[ \t]+([0-9]+)" _minor_version_match "${_rs_H_CONTENTS}")
+  set(REALSENSE_API_MINOR_VERSION "${CMAKE_MATCH_1}")
+  string(REGEX MATCH "define[ \t]+RS_API_PATCH_VERSION[ \t]+([0-9]+)" _patch_version_match "${_rs_H_CONTENTS}")
+  set(REALSENSE_API_PATCH_VERSION "${CMAKE_MATCH_1}")
+  set(REALSENSE_API_VERSION "${REALSENSE_API_MAJOR_VERSION}.${REALSENSE_API_MINOR_VERSION}.${REALSENSE_API_PATCH_VERSION}")
+  unset(_rs_H_CONTENTS)
+
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RealSense
+  FOUND_VAR REALSENSE_FOUND
+  REQUIRED_VARS REALSENSE_LIBRARY REALSENSE_INCLUDE_DIR
+  VERSION_VAR REALSENSE_API_VERSION
+)
+
