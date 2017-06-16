f731d84abf0b727443b59f8a6c410548aeab02d1
Sergey Alexandrov <alexandrov88@gmail.com>
2017-06-16 15:15:08 +0200

    Compile Ceres with GLOG and SuiteSparse on Travis

--- .travis.sh
+++ .travis.sh
@@ -3,6 +3,7 @@ RADICAL_BUILD_DIR=$RADICAL_DIR/build
 OPENCV_DIR=$HOME/opencv
 CMAKE_DIR=$HOME/cmake
 EIGEN_DIR=$HOME/eigen
+GLOG_DIR=$HOME/glog
 CERES_DIR=$HOME/ceres
 DOWNLOAD_DIR=$HOME/download
 
@@ -129,6 +130,35 @@ function install_eigen()
   return $?
 }
 
+function install_glog()
+{
+  local pkg_ver=${GLOG_VERSION}
+  local pkg_url="https://github.com/google/glog/archive/v${pkg_ver}.tar.gz"
+  local pkg_md5sum="5df6d78b81e51b90ac0ecd7ed932b0d4"
+  local pkg_src_dir=${DOWNLOAD_DIR}/glog-${pkg_ver}
+  local pkg_install_dir=${GLOG_DIR}/${pkg_ver}
+  local pkgconfig=${pkg_install_dir}/lib/pkgconfig/libglog.pc
+  echo "Installing GLOG ${pkg_ver}"
+  if [[ -e ${pkgconfig} ]]; then
+    local version=`grep -Po "(?<=Version: )[0-9.]*" ${pkgconfig}`
+    if [[ "${version}" = "$pkg_ver" ]]; then
+      local modified=`stat -c %y ${pkgconfig} | cut -d ' ' -f1`
+      echo " > Found cached installation of GLOG"
+      echo " > Version ${pkg_ver}, built on ${modified}"
+      return 0
+    fi
+  fi
+  download ${pkg_url} ${pkg_md5sum}
+  if [[ $? -ne 0 ]]; then
+    return $?
+  fi
+  tar xzf pkg
+  cd ${pkg_src_dir}
+  ./configure --prefix=$pkg_install_dir && make -j4 && make install
+  make -j2 && make install && touch ${pkgconfig}
+  return $?
+}
+
 function install_ceres()
 {
   local pkg_ver=${CERES_VERSION}
@@ -158,7 +188,8 @@ function install_ceres()
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$pkg_install_dir \
     -DEIGEN_INCLUDE_DIR=$EIGEN_DIR/$EIGEN_VERSION/include/eigen3 \
-    -DMINIGLOG=ON \
+    -DGLOG_INCLUDE_DIR=$GLOG_DIR/$GLOG_VERSION/include \
+    -DGLOG_LIBRARY=$GLOG_DIR/$GLOG_VERSION/lib/libglog.so \
     -DBUILD_EXAMPLES=OFF \
     -DBUILD_TESTING=OFF
   make -j2 && make install && touch ${cmake_config}
@@ -185,7 +216,8 @@ function download()
 
 CMAKE_VERSION="2.8.12.2"
 EIGEN_VERSION="3.2.10"
+GLOG_VERSION="0.3.5"
 CERES_VERSION="1.10.0"
 export PATH=$CMAKE_DIR/$CMAKE_VERSION/bin:$PATH
 
-install_cmake && install_opencv && install_eigen && install_ceres && build && test
+install_cmake && install_opencv && install_eigen && install_glog && install_ceres && build && test
